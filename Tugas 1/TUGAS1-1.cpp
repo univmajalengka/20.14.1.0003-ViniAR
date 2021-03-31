8875805a2fd8b1af58f8ddc65a18969ea469d695
viniarifianir16 <73510705+viniarifianir16@users.noreply.github.com>
2021-03-31 08:32:55 +0700

    Add files via upload

---
+++ Tugas 1/TUGAS1-1.cpp
@@ -0,0 +1,37 @@
+/*
+Nama			: Vini Arifiani Rohmat
+NPM				: 20.14.1.0003
+Nama Program	: TUGAS1-1.cpp
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string nama,npm,alamat,ttl,no_hp,motto;
+	
+	cout<<"Masukkan Nama   : ";
+	getline(cin, nama);
+	cout<<"Masukkan NPM    : ";
+	getline(cin, npm);
+	cout<<"Masukkan Alamat : ";
+	getline(cin, alamat);
+	cout<<"Masukkan TTL    : ";
+	getline(cin, ttl);
+	cout<<"Masukkan No.HP  : ";
+	getline(cin, no_hp);
+	cout<<"Masukkan Motto  : ";
+	getline(cin, motto);
+	
+	cout<<"--------------------------------"<<endl;
+	cout<<endl;
+	cout<<"Nama   : "<<nama<<endl;	
+	cout<<"NPM    : "<<npm<<endl;
+	cout<<"Alamat : "<<alamat<<endl;
+	cout<<"TTL    : "<<ttl<<endl;
+	cout<<"No.HP  : "<<no_hp<<endl;
+	cout<<"Motto  : "<<motto<<endl;
+}
